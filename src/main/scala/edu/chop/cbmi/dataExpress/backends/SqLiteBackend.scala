a28cd80ab0751095c2cbd43d2e2d494a7a5c7402
Michael Italia <italiam@email.chop.edu>
2012-10-19 16:19:26 -0400

    Updated sqlite backend

--- src/main/scala/edu/chop/cbmi/dataExpress/backends/SqLiteBackend.scala
+++ src/main/scala/edu/chop/cbmi/dataExpress/backends/SqLiteBackend.scala
@@ -21,6 +21,7 @@ USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 package edu.chop.cbmi.dataExpress.backends
 import edu.chop.cbmi.dataExpress.dataModels.DataRow
 import java.util.Properties
+import java.sql.PreparedStatement
 
 
 class SqLiteBackend(override val connectionProperties : Properties, _sqlDialect : SqlDialect = null, _driverClassName : String = null)
@@ -33,5 +34,23 @@ class SqLiteBackend(override val connectionProperties : Properties, _sqlDialect 
     DataRow.empty
   }
   
+
   override val SUPPORTS_MULT_RS = false
+
+  override protected def prepStatement(sqlStatement: PreparedStatement, bindVars: Seq[Option[_]]) = {
+    if (bindVars.length > 0) {
+      val vars = bindVars.zipWithIndex
+      for (v <- vars) {
+        //TODO: Too many edge cases in here, need to explicity set some more date/time stuff
+        v._1 match {
+          case Some(i: java.sql.Timestamp)    => sqlStatement.setString((v._2 + 1), i.toString())
+          case Some(i: java.sql.Time)         => sqlStatement.setString((v._2 + 1), i.toString())
+          //TODO: Test the java.util.Date for precision here to avoid trying to set to a higher precision
+          case Some(i: java.util.Date)        => sqlStatement.setString((v._2 + 1), java.text.DateFormat.getDateTimeInstance().format(i))
+          case None => sqlStatement.setNull(v._2 + 1, java.sql.Types.NULL)  //TODO: do something better here
+          case _    => sqlStatement.setObject((v._2 + 1), v._1.get)
+        }
+      }
+    }
+  }
 }
