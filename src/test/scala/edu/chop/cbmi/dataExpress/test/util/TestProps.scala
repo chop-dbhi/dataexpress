9b8c9b4c1f5f88acf62a6bc557470fe6c355bbf2
Michael Italia <italiam@email.chop.edu>
2012-11-19 09:27:46 -0500

    Fixed so that test builds properly locate the properties files

--- src/test/scala/edu/chop/cbmi/dataExpress/test/util/TestProps.scala
+++ src/test/scala/edu/chop/cbmi/dataExpress/test/util/TestProps.scala
@@ -1,6 +1,7 @@
 package edu.chop.cbmi.dataExpress.test.util
 
 import java.io.InputStream
+import scala.io.Source
 import java.util.Properties
 import edu.chop.cbmi.dataExpress.backends.{SqlBackendFactory, PostgresBackend, SqlBackend}
 
@@ -13,25 +14,26 @@ import edu.chop.cbmi.dataExpress.backends.{SqlBackendFactory, PostgresBackend, S
  */
 
 object TestProps {
-  def getDbProps(dbName:String) = {
+  def getDbProps(dbName:String):Properties = {
     val filename = choosePropFile(dbName)
-	val inputStream = this.getClass().getResourceAsStream(filename)
-    val props = new Properties()
+	val inputStream =(getClass.getResourceAsStream(filename))
+    //val inputStream = this.getClass().getResourceAsStream(filename)
+	val props = new Properties()
     props.load(inputStream)
     props
   }
 
   def getDbPropFilePath(dbName:String) = { 
    val filename = choosePropFile(dbName)
-   this.getClass().getResource(filename).getPath()
+   getClass().getResource(filename).getPath
  }
   
   private def choosePropFile(dbname: String) = {
     dbname match {
-      case "mysql" => "mysql_test.properties"
-      case "postgres" => "postgres_test.properties"
-      case "sqlite" => "sqlite_test.properties"
-      case _ => "%s.properties".format(dbname)
+      case "mysql" => "/mysql_test.properties"
+      case "postgres" => "/postgres_test.properties"
+      case "sqlite" => "/sqlite_test.properties"
+      case _ => "/%s.properties".format(dbname)
     }
   }
 
