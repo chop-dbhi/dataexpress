2b528120b2fed87b10a4dd2a132b662aeabb0185
Michael Italia <italiam@email.chop.edu>
2013-01-17 15:37:27 -0500

    Eliminate SqlQueryPackage
    Functionality in SqlQueryPackage has now been separated into the
    SqlMetadata trait as well as SqlRelation. As mentioned in other
    commits, the primary driver for this is the need to centralize the
    iteration over JDBC ResultSets and also to try as hard as humanly
    possible not to execute multiple simultaneous database queries. All
    metadata from the Metadata trait is marked as lazy to ensure that
    nothing is executed until the last possible moment. This also
    eliminates some duplicated state and metadata held in the SqlIterator
    and query packages.

--- src/main/scala/edu/chop/cbmi/dataExpress/dataModels/sql/SqlRelation.scala
+++ src/main/scala/edu/chop/cbmi/dataExpress/dataModels/sql/SqlRelation.scala
@@ -8,129 +8,88 @@ import edu.chop.cbmi.dataExpress.dataModels._
 import java.sql.{ResultSetMetaData, ResultSet}
 import scala.language.dynamics
 
-/**
- * Wrapper for queries that produce a JDBC ResultSet
- * 
- * @param dataStore A [[edu.chop.cbmi.dataExpress.backends.SqlBackend]] with an open connection, ready for queries
- * @param query A string representation of a SQL query, optionally with placeholder values for bind variables
- * @param bindVars An optional set of values to be bound when making a prepared statement
- * 
- */
-case class SqlQueryPackage(val dataStore : SqlBackend, val query : String, val bindVars : Seq[Option[_]]) extends ColumnNameGenerator{
-  private var _resultSet : Option[ResultSet] = None
-  private var _meta : Option[ResultSetMetaData] = None
-  private var _columnCount : Option[Int] = None
-  private var _columnNames : Option[Seq[String]] = None
-  private var _dataTypes : Option[Seq[DataType]] = None
 
-  def resultSet() : ResultSet = {
-    _resultSet match{
-      case Some(rs) => rs
-      case _ =>{
-        val rs = dataStore.executeQuery(query, bindVars)
-        _resultSet = Some(rs)
-        rs
-      }
-    }
-  }
 
-  def meta() : ResultSetMetaData = {
-    _meta match{
-      case Some(m) => m
-      case _ => {
-        val m = resultSet.getMetaData
-        _meta = Some(m)
-        m
-      }
-    }
-  }
-
-  def columnCount() : Int = {
-    _columnCount match {
-      case Some(cc) => cc
-      case _ => {
-       val cc = meta.getColumnCount
-        _columnCount = Some(cc)
-        cc
-      }
-    }
-  }
-
-  def columnNames() : Seq[String] = {
-    _columnNames match{
-      case Some(cn) => cn
-      case _ =>{
-        val cn = (1 to columnCount) map {(i:Int)=>meta.getColumnLabel(i)}
-        _columnNames = Some(cn)
-        cn
-      }
-    }
-  }
-
-  def dataTypes() : Seq[DataType] = {
-    val m = meta
-    val cn = columnNames
-    if(!dataStore.SUPPORTS_MULT_RS){
-      _resultSet match{
-        case Some(rs) => rs.close()
-        case _ => {}
-      }
-      _resultSet = None
-    }
-    //val dt = dataStore.sqlDialect.mapDataTypes(cn,m)
-    //dt
-    dataStore.sqlDialect.mapDataTypes(cn,m)
-  }
-
-  def generate_column_names() = columnNames
-}
 
 /**
  * A [[edu.chop.cbmi.dataExpress.dataModels.DataTable]] that represents a SQL relation (either a table or
  * the complete result of a query).
  * 
  */
-sealed case class SqlRelation[+T] private[dataModels](private val sql_query_package : SqlQueryPackage)
-  extends DataTable[T](sql_query_package) {
 
-  lazy private val sqlMeta = sql_query_package.meta
-  lazy private val columnCount = sql_query_package.columnCount
-  lazy val dataTypes = sql_query_package.dataTypes
-  lazy private val query_package = SqlQueryPackage(sql_query_package.dataStore, sql_query_package.query, sql_query_package.bindVars)
-  lazy private val iterator = SqlRelationIterator(query_package)
+trait SqlMetadata extends Metadata {
+  val resultSetMetadata:java.sql.ResultSetMetaData
+}
+
+sealed case class SqlRelation[+T](query:String, bindVars:Seq[Option[_]],  backend:SqlBackend) extends DataTable[T] with SqlMetadata{
+  	override lazy val columnNames:Seq[String] = (1 to columnCount).map{resultSetMetadata.getColumnLabel(_)}.toSeq
+	override lazy val dataTypes:Seq[DataType] = backend.sqlDialect.mapDataTypes(columnNames,resultSetMetadata)
+	override lazy val columnCount:Int = resultSetMetadata.getColumnCount()
+	override lazy val resultSetMetadata = resultSet.getMetaData()
+	lazy private val resultSet = backend.executeQuery(query, bindVars)
+	lazy private val iterator = SqlRelationIterator(this)
   
   override def next() = iterator.next()
   
   override def hasNext = iterator.hasNext
 
   //TODO: This needs to be in a SQL dialect if at all possible
-  private def sub_query(name : String) =
-    """SELECT  """ + name + """ FROM (""" + sql_query_package.query + """) AS """ + name
+  def sub_query(name : String) = """SELECT  %s FROM (%s) AS %s""".format(name, query,name)
 
-  override def col(name:String) = if(hasColumn(name)){
-    def f(item:Any) = if(item==null) None else Some(item.asInstanceOf[T])
-    SqlRelationColumn[Option[T]](SqlQueryPackage(sql_query_package.dataStore, sub_query(name), sql_query_package.bindVars), f _)
-  }else throw ColumnDoesNotExist(name)
 
-  override def col_as[G](name: String)(implicit m: Manifest[G]) = if(hasColumn(name)){
-    def f(a:Any) = if(a==null)None else Some(a).as[G]
-    SqlRelationColumn[Option[G]](SqlQueryPackage(sql_query_package.dataStore, sub_query(name), sql_query_package.bindVars), f _)
-  }else throw ColumnDoesNotExist(name)
+  override def col(name:String) = {
+	  if(hasColumn(name)) SqlRelation(sub_query(name), bindVars, backend).map{r:DataRow[T] => r(0)}
+      else throw ColumnDoesNotExist(name)
+	}
 
-  override def col_asu[G](name: String)(implicit m: Manifest[G]) = if(hasColumn(name)){
-    def f(a:Any) = if(a==null)None.asu[G] else Some(a).asu[G]
-    SqlRelationColumn[G](SqlQueryPackage(sql_query_package.dataStore, sub_query(name), sql_query_package.bindVars), f _)
-  }else throw ColumnDoesNotExist(name)
-  
+  override def col_as[G](name: String)(implicit m: Manifest[G]) = {
+    if(hasColumn(name)) SqlRelation(sub_query(name), bindVars, backend).map{r:DataRow[G] => r(0).as[G]}
+  }
 
+  override def col_asu[G](name: String)(implicit m: Manifest[G]) = {
+    if(hasColumn(name)) SqlRelation(sub_query(name), bindVars, backend).map{r:DataRow[_] => r(0).asu[G]}
+    else throw ColumnDoesNotExist(name)
+	}
  
-  case class SqlRelationIterator[+T] private[SqlRelation](val sql_query_package : SqlQueryPackage)
-    extends SqlIterator[DataRow[T]](sql_query_package){
+  case class SqlRelationIterator[+T] (val sqlRelation : SqlRelation[T]) extends Iterator[DataRow[T]] {
 
-    lazy private val column_names = sql_query_package.generate_column_names()
-    lazy private val column_count = sql_query_package.columnCount
+    lazy private val column_names = sqlRelation.columnNames
+    lazy private val column_count = sqlRelation.columnCount
+    protected var cursor_advanced = false
+    protected var more_rows = false
+    
+    
+  override def hasNext() = {
+    if (!cursor_advanced) {
+      cursor_advanced = true
+      more_rows = sqlRelation.resultSet.next()
+    }
+    more_rows
+  }
 
-    override def generate_next(): DataRow[T] = {
+  override def next() = {
+    if (cursor_advanced) cursor_advanced = false
+    else more_rows = sqlRelation.resultSet.next()
+    //Some databases will complain if a result set is not properly closed, need to generate the next set of values
+    //then close the result set.
+    val nextRow = generate_next()
+    if(!hasNext) {
+      sqlRelation.resultSet.close()
+    }
+    nextRow
+  }
+
+
+  def next_item_in_column(i: Int) = sqlRelation.resultSetMetadata.getColumnType(i) match {
+    //Postgres Boolean values such as 't' were having issues
+    //because they are mapped to java.sql.Types.BIT
+    //TODO: this code needs to be moved out of here
+    case java.sql.Types.BIT => sqlRelation.resultSet.getBoolean(i)
+    case _ => sqlRelation.resultSet.getObject(i)
+  }
+    
+    
+    def generate_next():DataRow[T] = {
       val row = (1 to column_count) map (next_item_in_column(_))
       val dr = DataRow(column_names)(row map ((x: Any) => if (x == null) None else Some(x.asInstanceOf[T])))
       //This may not be the best spot to perform this operation, but it should suffice for the current implementation
