ab9320dde1198d94e6353c95f97a14d5b0130956
Michael Italia <italiam@email.chop.edu>
2013-04-04 16:19:03 -0400

    Comment out column slice support

--- src/main/scala/edu/chop/cbmi/dataExpress/dataModels/sql/SqlTransform.scala
+++ src/main/scala/edu/chop/cbmi/dataExpress/dataModels/sql/SqlTransform.scala
@@ -6,7 +6,7 @@ import edu.chop.cbmi.dataExpress.dataModels.{DataTable,DataRow}
  * SqlTransform provides a formal mechanism for transforming the rows of tables.
  * SqlTransform wraps another DataTable and applies a
  * function which transforms the rows of the table. The result is a new
- * DataTable which may columns that differ in number and type from the
+ * DataTable which may have columns that differ in number and type from the
  * source table.
  * 
  * @param sourceTable The table where rows will be drawn
@@ -14,14 +14,12 @@ import edu.chop.cbmi.dataExpress.dataModels.{DataTable,DataRow}
  */
 
 case class SqlTransform[T](sourceTable:DataTable[_])(transformation:DataRow[_] => DataRow[T]) extends DataTable[T] {
-	/** As seen from class SqlTransform, the missing signatures are as follows. 
-	 * *  For convenience, these are usable as stub implementations.  */   
-  // Members declared in edu.chop.cbmi.dataExpress.dataModels.DataTable   
+
 
   //Considering moving these out as a trait or completely eliminating
-  override def col(name: String): Iterator[Option[T]] = ???
-  def col_as[G](name: String)(implicit m: scala.reflect.Manifest[G]): Iterator[Option[G]] = ???
-  def col_asu[G](name: String)(implicit m: scala.reflect.Manifest[G]): Iterator[G] = ???
+//  override def col(name: String): Iterator[Option[T]] = ???
+//  def col_as[G](name: String)(implicit m: scala.reflect.Manifest[G]): Iterator[Option[G]] = ???
+//  def col_asu[G](name: String)(implicit m: scala.reflect.Manifest[G]): Iterator[G] = ???
   
   // Members declared in edu.chop.cbmi.dataExpress.dataModels.Metadata   
   lazy val columnCount: Int = columnNames.length  
@@ -61,7 +59,7 @@ case class SqlTransform[T](sourceTable:DataTable[_])(transformation:DataRow[_] =
   private def getColumns:Seq[String] = {
     nextRow match {
       case Some(row) => row.columnNames
-      //Calling hasNext purely for side effects is not a not great solution to a bad situation in general
+      //Calling hasNext purely for side effects is a "not great" solution to a bad situation in general
       case None => if(hasNext()) { 
     	  nextRow match {
     	    case Some(row) => row.columnNames
