a5855ec6c64b0ec545b295b94ca3019b05738794
Michael Italia <italiam@email.chop.edu>
2013-04-04 16:17:15 -0400

    Add DataWriter trait to SQL Store

--- src/main/scala/edu/chop/cbmi/dataExpress/dsl/stores/Store.scala
+++ src/main/scala/edu/chop/cbmi/dataExpress/dsl/stores/Store.scala
@@ -1,32 +1,7 @@
-/*
-Copyright (c) 2012, The Children's Hospital of Philadelphia All rights reserved.
-
-Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
-following conditions are met:
-
-1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following
-   disclaimer.
-
-2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
-   following disclaimer in the documentation and/or other materials provided with the distribution.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
-INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
-USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 package edu.chop.cbmi.dataExpress.dsl.stores
-
-/**
- * Created by IntelliJ IDEA.
- * User: masinoa
- * Date: 12/28/11
- * Time: 8:43 AM
- * To change this template use File | Settings | File Templates.
- */
+import edu.chop.cbmi.dataExpress.dataModels._
+import edu.chop.cbmi.dataExpress.dataWriters.OperationStatus
+import beans.BooleanBeanProperty
 
 trait Store {
 
@@ -37,6 +12,32 @@ trait Store {
   def close : Boolean
 
   def save : Boolean
+
+  def createTable(name:String, table:DataTable[_]) : Boolean
+
+  def insertRow(tableName : String, row : DataRow[_]) : Boolean
+
+  def insertRow(tableName : String, f : (String)=>Option[_]) : Boolean
+
+  /**
+   *
+   * @param tableName The name of the table to insert the rows
+   * @param table A source table that contains the rows to be inserted
+   * @return number of rows successfully inserted
+   */
+  def insertRows(tableName : String, table : DataTable[_]) : Int
+
+  /**
+   * @param updated_row DataRow containing updated values
+   * @param filter key,value pairs used to identify the row to update
+   */
+  def updateRow(table_name : String, updated_row : DataRow[_], filter : (String,_)*) : Boolean
+
+  /**
+   * @param filter key,value pairs used to identify the row to update
+   * @param f a function that maps the column_names in the table to the new values for the updated row
+   */
+  def updateRow(table_name : String, filter : (String,_)*)(f:(String)=>Option[_]) : OperationStatus
 
   def unique_id : Any
 
