a147c0c1cd969fb07915fb544958a935f14c57a4
Michael Italia <italiam@email.chop.edu>
2013-04-04 16:18:39 -0400

    Make capitalization consistent

--- src/main/scala/edu/chop/cbmi/dataExpress/dataWriters/DataWriter.scala
+++ src/main/scala/edu/chop/cbmi/dataExpress/dataWriters/DataWriter.scala
@@ -14,25 +14,25 @@ import sql.SqlTableWriter
 
 trait DataWriter {
 
-  def insert_row[T](table_name : String, row : DataRow[T]) : OperationStatus
+  def insertRow[T](table_name : String, row : DataRow[T]) : OperationStatus
 
-  def insert_row[T](table_name : String, f : (String)=>Option[T]) : OperationStatus
+  def insertRow[T](table_name : String, f : (String)=>Option[T]) : OperationStatus
 
-  def insert_rows[T](table_name : String, table : DataTable[T]) : OperationStatus
+  def insertRows[T](table_name : String, table : DataTable[T]) : OperationStatus
 
   /**
    * @param updated_row DataRow containing updated values
    * @param filter key,value pairs used to identify the row to update
    */
-  def update_row[T](table_name : String, updated_row : DataRow[T], filter : (String,_)*) : OperationStatus
+  def updateRow[T](table_name : String, updated_row : DataRow[T], filter : (String,_)*) : OperationStatus
 
   /**
    * @param filter key,value pairs used to identify the row to update
    * @param f a function that maps the column_names in the table to the new values for the updated row
    */
-  def update_row[T](table_name : String, filter : (String,_)*)(f:(String)=>Option[T]) : OperationStatus
+  def updateRow[T](table_name : String, filter : (String,_)*)(f:(String)=>Option[T]) : OperationStatus
 
-  def insert_table[T,G<:DataType](table_name : String, data_types : Seq[G] = Seq.empty[DataType], table : DataTable[T] = DataTable.empty, overwrite_option:Int = -1) : OperationStatus
+  def insertTable[T,G<:DataType](table_name : String, data_types : Seq[G] = Seq.empty[DataType], table : DataTable[T] = DataTable.empty, overwrite_option:Int = -1) : OperationStatus
 }
 
 /**Factory method for creating instances of [[edu.chop.cbmi.dataExpress.dataWriters.DataWriter]]*/
