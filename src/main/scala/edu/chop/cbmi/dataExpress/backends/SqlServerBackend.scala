42ba67c5b1fb59d90ac29570ef31ec4863ab1f97
Michael Italia <ITALIAM@email.chop.edu>
2013-07-31 14:29:58 -0400

    Add doc for SQLServer connection

--- src/main/scala/edu/chop/cbmi/dataExpress/backends/SqlServerBackend.scala
+++ src/main/scala/edu/chop/cbmi/dataExpress/backends/SqlServerBackend.scala
@@ -2,6 +2,19 @@ package edu.chop.cbmi.dataExpress.backends
 
 import java.util.Properties
 
+/**
+ * Backend for accessing SqlServer databases. This will use a connection properties file that
+ * should look something like the following:
+ * {{{driverClassName=com.microsoft.sqlserver.jdbc.SQLServerDriver
+jdbcUri=jdbc:sqlserver://server_address:server_port
+database=database_name
+user=user_name
+password=password
+}}}
+ *
+ */
+
+
 class SqlServerBackend(override val connectionProperties : Properties, _sqlDialect : SqlDialect = null,
                        _driverClassName : String = null)
   extends SqlBackend(connectionProperties, if(_sqlDialect==null)SqlServerSqlDialect else _sqlDialect,
