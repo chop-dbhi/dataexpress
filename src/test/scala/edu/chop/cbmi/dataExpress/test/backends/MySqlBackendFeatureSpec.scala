0558b308439f122fc85d6dce92633118dd639212
Michael Italia <ITALIAM@email.chop.edu>
2013-08-02 15:33:35 -0400

    Remove test data teardown to see if it helps travis ci

--- src/test/scala/edu/chop/cbmi/dataExpress/test/backends/MySqlBackendFeatureSpec.scala
+++ src/test/scala/edu/chop/cbmi/dataExpress/test/backends/MySqlBackendFeatureSpec.scala
@@ -63,12 +63,12 @@ class MySqlBackendFeatureSpec extends FeatureSpec with GivenWhenThen with Should
 
 
   def removeTestDataSetup: Boolean = {
-
-    setup.targetBackend.close()
-    setup.targetBackend.connect()
-    setup.targetBackend.execute(setup.dataSetup.dropTargetSchema)
-    setup.targetBackend.commit
-    setup.targetStatement.close()
+//
+//    setup.targetBackend.close()
+//    setup.targetBackend.connect()
+//    setup.targetBackend.execute(setup.dataSetup.dropTargetSchema)
+//    setup.targetBackend.commit
+//    setup.targetStatement.close()
     true
   }
 
