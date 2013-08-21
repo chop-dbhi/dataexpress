dc47aaf0ff9afbed212e0a825ab049e672d6d41d
Michael Italia <ITALIAM@email.chop.edu>
2013-08-21 17:11:38 -0400

    Wrap individual statements with commit_on_success
    This test was failing because there were stealth transactions fouling
    up the commit_on_success. It's not practical to wrap so much in a
    commit_on_success, given the need to potentially close
    statements/connections and do other work. It's likely transaction
    management needs to be better specificed.

--- src/test/scala/edu/chop/cbmi/dataExpress/test/dsl/CopySpec.scala
+++ src/test/scala/edu/chop/cbmi/dataExpress/test/dsl/CopySpec.scala
@@ -23,8 +23,8 @@ import edu.chop.cbmi.dataExpress.test.util.presidents.SQLStatements.potus_data_r
 
 class CopySpec extends PresidentsSpecWithSourceTarget {
 
-  override val backend_test_type : KNOWN_SQL_BACKEND = POSTGRES()
-  //override val backend_test_type : KNOWN_SQL_BACKEND = MYSQL()
+  //override val backend_test_type : KNOWN_SQL_BACKEND = POSTGRES()
+  override val backend_test_type : KNOWN_SQL_BACKEND = MYSQL()
 
   //will be creating some extra tables
   val VICE_PRESIDENTS = add_known_table("vice_presidents")
@@ -44,20 +44,21 @@ class CopySpec extends PresidentsSpecWithSourceTarget {
     it("should copy tables"){
       val source_db = "source_db"
       val target_db = "target_db"
-      ETL.execute(true, true){
+      ETL.execute(on = true, do_cleanup = true){
     	  //was prop_file_target
         register store SqlDb(prop_file_target, schema) as target_db
         register store SqlDb(prop_file_source, schema) as source_db
-
-        commit_on_success(target_db){
-          copy table PRESIDENTS from source_db to target_db create PC_ONE
-
+          commit_on_success(target_db) {
+            copy table PRESIDENTS from source_db to target_db create PC_ONE
+          }
           AssertionOps.assertion_functions += ( () => query_and_count(PC_ONE, targetSchema = schema) should equal(default_president_count))
           BackendOps.add_table_name(target_backend, PC_ONE, schema)
           val init_pres_map = Map(1->potus_data_row(1), 2->potus_data_row(2), 3->potus_data_row(3), 4->potus_data_row(4))
           add_compare_table_assertion(PC_ONE, init_pres_map)
 
-          copy table PRESIDENTS from source_db change_column_names("first_name"->"FN", "last_name"->"LN") to target_db create PC_TWO
+          commit_on_success(target_db){
+            copy table PRESIDENTS from source_db change_column_names("first_name"->"FN", "last_name"->"LN") to target_db create PC_TWO
+          }
           BackendOps.add_table_name(target_backend, PC_TWO, schema)
           add_compare_table_assertion(PC_TWO, init_pres_map, (query_row: DataRow[_], map_row : DataRow[_]) => {
             query_row.FN should equal(map_row.first_name)
@@ -67,21 +68,26 @@ class CopySpec extends PresidentsSpecWithSourceTarget {
             query_row.id should equal(map_row.id)
           })
 
-          copy table PRESIDENTS from source_db filter {row => row.num_terms.asu[Int] == 2} to target_db create TTP_ONE
+          commit_on_success(target_db){
+            copy table PRESIDENTS from source_db filter {row => row.num_terms.asu[Int] == 2} to target_db create TTP_ONE
+          }
           BackendOps.add_table_name(target_backend, TTP_ONE, schema)
           val ttp_map = Map(1->potus_data_row(1), 3->potus_data_row (3), 4->potus_data_row (4))
           add_compare_table_assertion(TTP_ONE, ttp_map)
 
-          copy table PRESIDENTS from source_db transform {row =>
-            val new_id = row.id.asu[Int] * 100
-            new_row(
-              "id" -> new_id,
-              "first_name" -> row.first_name.asu[String].toUpperCase,
-              "last_name" -> row.last_name.asu[String].toUpperCase,
-              "num_terms" -> row.num_terms.asu[Int],
-              "dob" -> row.dob.as[Date].getOrElse(null)
-            )
-          } to target_db create UPPER_PRES_COPY
+          commit_on_success(target_db){
+            copy table PRESIDENTS from source_db transform {row =>
+              val new_id = row.id.asu[Int] * 100
+              new_row(
+                "id" -> new_id,
+                "first_name" -> row.first_name.asu[String].toUpperCase,
+                "last_name" -> row.last_name.asu[String].toUpperCase,
+                "num_terms" -> row.num_terms.asu[Int],
+                "dob" -> row.dob.as[Date].getOrElse(null)
+              )
+            } to target_db create UPPER_PRES_COPY
+          }
+
           BackendOps.add_table_name(target_backend, UPPER_PRES_COPY, schema)
           add_compare_table_assertion(UPPER_PRES_COPY, init_pres_map, (query_row: DataRow[_], map_row: DataRow[_]) => {
             query_row.first_name.asu[String] should equal(map_row.first_name.asu[String].toUpperCase)
@@ -91,17 +97,19 @@ class CopySpec extends PresidentsSpecWithSourceTarget {
             query_row.id.asu[Int] should equal(map_row.id.asu[Int]*100)
           }, (i:Int)=>i/100)
 
-          copy table PRESIDENTS from source_db alter {table =>
-            table set_data_types(IntegerDataType, CharacterDataType(20, false))
-            table set_column_names("id", "name")
-            table set_row_values {row =>
-              val collapsed_name = row.last_name.asu[String] + ", " + row.first_name.asu[String]
-              new_row(
-                "id" -> row.id.asu[Int],
-                "name" -> collapsed_name
-              )
-            }
-          } to target_db create PRES_COLLAPSED_NAMES
+          commit_on_success(target_db){
+            copy table PRESIDENTS from source_db alter {table =>
+              table set_data_types(IntegerDataType, CharacterDataType(20, false))
+              table set_column_names("id", "name")
+              table set_row_values {row =>
+                val collapsed_name = row.last_name.asu[String] + ", " + row.first_name.asu[String]
+                new_row(
+                  "id" -> row.id.asu[Int],
+                  "name" -> collapsed_name
+                )
+              }
+            } to target_db create PRES_COLLAPSED_NAMES
+          }
           BackendOps.add_table_name(target_backend, PRES_COLLAPSED_NAMES, schema)
           AssertionOps.assertion_functions += (() => {
             val t = DataTable(source_backend, """select * from %s.%s""".format(
@@ -119,32 +127,42 @@ class CopySpec extends PresidentsSpecWithSourceTarget {
 
           val sql_statement = """select * from %s.%s where %s='2'""".format(schema.get, PRESIDENTS,
             source_backend.sqlDialect.quoteIdentifier("num_terms"))
-          copy query sql_statement from source_db to target_db create TTP_TWO
+
+          commit_on_success(target_db) {
+            copy query sql_statement from source_db to target_db create TTP_TWO
+          }
           BackendOps.add_table_name(target_backend, TTP_TWO, schema)
           add_compare_table_assertion(TTP_TWO, ttp_map)
 
           val bindable_statement = """select * from %s.%s where %s= ?""".format(schema.get, PRESIDENTS,
             source_backend.sqlDialect.quoteIdentifier("num_terms"))
-          copy query bindable_statement using_bind_vars 2 from source_db to target_db create TTP_THREE
+          commit_on_success(target_db) {copy query bindable_statement using_bind_vars 2 from source_db to target_db create TTP_THREE}
           BackendOps.add_table_name(target_backend, TTP_THREE, schema)
           add_compare_table_assertion(TTP_THREE, ttp_map)
 
-          copy table UPPER_PRES_COPY from source_db to target_db create TTP_FOUR
+          commit_on_success(target_db) {
+            copy table UPPER_PRES_COPY from source_db to target_db create TTP_FOUR
+          }
           val rs = target_backend.executeQuery("select count(*) from %s.ttp_four".format(schema.get))
           rs.next()
           val num = rs.getInt(1)
           num should be > 0
           val sql_statement2 = """select * from %s.%s""".format(schema.get, PRESIDENTS)
-          copy query sql_statement2 from source_db to target_db append TTP_FOUR
+          commit_on_success(target_db)
+          {
+            copy query sql_statement2 from source_db to target_db append TTP_FOUR
+          }
           BackendOps.add_table_name(target_backend, TTP_FOUR, schema)
           AssertionOps.query_and_count(TTP_FOUR, target_backend, schema) should equal(2*default_president_count)
 
-          copy table UPPER_PRES_COPY from source_db to target_db create TTP_FIVE
-          copy table PRESIDENTS from source_db to target_db append TTP_FIVE
+          commit_on_success(target_db) {
+            copy table UPPER_PRES_COPY from source_db to target_db create TTP_FIVE
+            copy table PRESIDENTS from source_db to target_db append TTP_FIVE
+          }
+
           BackendOps.add_table_name(target_backend, TTP_FIVE, schema)
           AssertionOps.query_and_count(TTP_FIVE, target_backend, schema) should equal(2*default_president_count)
-        }
-      } should equal(Left(true))
+      } // This was here, but with refactoring seems like it shoulndn't be anymore: should equal(Left(true))
 
       AssertionOps.execute_assertions()
     }
